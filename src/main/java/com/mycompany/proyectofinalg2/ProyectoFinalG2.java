12fb4ea8427ed7a2da743e08d3b10e561c3649fc
PLAlvarez11 <106171305+PLAlvarez11@users.noreply.github.com>
2023-05-21 21:11:40 -0600

    Creación del proyecto

---
+++ src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalg2;
+
+/**
+ *
+ * @author pluis
+ */
+public class ProyectoFinalG2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
