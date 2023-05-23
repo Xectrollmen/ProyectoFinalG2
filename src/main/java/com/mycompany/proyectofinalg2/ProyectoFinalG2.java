0127487cff1cb2486835110804cbc530771f2a32
Xectrollmen <rbranonsteeven@gmail.com>
2023-05-22 21:39:37 -0600

    fhnfg

--- src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
+++ src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectofinalg2;
 public class ProyectoFinalG2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("gfsdgff");
     }
 }
